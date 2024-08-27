f2d5386330aac25c59c1b9afd1c52722bb1ed906
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2024-08-27 08:39:19 -0300

    trtrtrt

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+let exemplo = "uma questão de pergunta";
+let exemplo2 = 23;
+let exemplo3 = false;
+let exemplo4 = true; 
+
+let exemploLista = [ "frase1","frase2","frase"];
+
+let exemploObjeto =  [
+    nome: "Kaique",
+    displina: [
+        "Alt",
+        "Alternativa 2"
+    ]
+
+    "Pergunta 2"
+];
