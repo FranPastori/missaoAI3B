4df82b75554cbb4984576951c0f3d224e4c3e2fb
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2024-08-27 08:19:29 -0300

    fdfd

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResulrado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
